bd674552f2cd659ef79924bc7dd8df20c4de6fab
DJ Schleen <djschleen@gmail.com>
2022-08-25 10:18:46 -0600

    feat: Stabilization and vulnerability display (#17)
    * Adds vulnerability struct for OSSIndex vuln details
    * Adds debug and enhanced summary output
    * Adds test case for removeDuplicates

--- cmd/scan.go
+++ cmd/scan.go
@@ -2,12 +2,14 @@ package cmd
 
 import (
 	"fmt"
+	"log"
 	"os"
 
 	"github.com/devops-kung-fu/common/util"
-	"github.com/fatih/color"
-	goocolor "github.com/gookit/color"
-	"github.com/rodaine/table"
+	"github.com/gookit/color"
+	"github.com/jedib0t/go-pretty/v6/table"
+	"github.com/jedib0t/go-pretty/v6/text"
+	"github.com/package-url/packageurl-go"
 	"github.com/spf13/cobra"
 
 	"github.com/devops-kung-fu/bomber/lib"
@@ -16,7 +18,8 @@ import (
 
 var (
 	token, username string
-	scanCmd         = &cobra.Command{
+	// summary, detailed bool
+	scanCmd = &cobra.Command{
 		Use:   "scan",
 		Short: "Scans a provided SBoM file or folder containing SBoMs for vulnerabilities.",
 		PreRun: func(cmd *cobra.Command, args []string) {
@@ -27,7 +30,7 @@ var (
 				token = os.Getenv("BOMBER_PROVIDER_TOKEN")
 			}
 			if username == "" && token == "" {
-				goocolor.Red.Println("Both a username and token are required\n")
+				color.Red.Println("Both a username and token are required\n")
 				_ = cmd.Help()
 				os.Exit(1)
 			}
@@ -39,44 +42,33 @@ var (
 				os.Exit(1)
 			}
 			if len(purls) > 0 {
-				util.PrintInfof("Scanning %v packages for vulnerabilities...\n\n", len(purls))
-			} else {
-				util.PrintInfo("No valid SBOMs were detected. Nothing has been scanned")
-			}
+				util.PrintInfof("Scanning %v packages for vulnerabilities...\n", len(purls))
+				util.PrintInfo("Vulnerability Provider:", providers.OutputCredits(), "\n")
 
-			providers.OutputCredits()
-			fmt.Println()
-
-			response, err := providers.OSSIndex(purls, username, token)
-			if err != nil {
-				util.PrintErr(err)
-				os.Exit(1)
-			}
-			vulnCount := 0
-			for _, r := range response {
-				vulns := len(r.Vulnerabilities)
-				vulnCount += vulns
-			}
-
-			if vulnCount > 0 {
-				headerFmt := color.New(color.FgGreen, color.Underline).SprintfFunc()
-				columnFmt := color.New(color.FgYellow).SprintfFunc()
-
-				tbl := table.New("purl", "description", "vulnerabilities")
-				tbl.WithHeaderFormatter(headerFmt).WithFirstColumnFormatter(columnFmt)
-
+				response, err := providers.OSSIndex(purls, username, token)
+				if err != nil {
+					util.PrintErr(err)
+					os.Exit(1)
+				}
+				vulnCount := 0
 				for _, r := range response {
 					vulns := len(r.Vulnerabilities)
-					if vulns > 0 {
-						tbl.AddRow(r.Coordinates, r.Description, vulns)
-					}
+					vulnCount += vulns
 				}
 
-				tbl.Print()
-				fmt.Println()
-				goocolor.Red.Printf("Vulnerabilities found: %v\n\n", vulnCount)
-			} else if vulnCount > 0 && len(response) > 0 {
-				goocolor.Green.Println("No vulnerabilities found!\n")
+				if vulnCount > 0 {
+					// if summary {
+					RenderSummary(response)
+					// } else if detailed {
+					// RenderDetails(response)
+					// }
+					fmt.Println()
+					color.Red.Printf("Vulnerabilities found: %v\n\n", vulnCount)
+				} else {
+					color.Green.Println("No vulnerabilities found!\n")
+				}
+			} else {
+				util.PrintInfo("No packages were detected. Nothing has been scanned.")
 			}
 
 		},
@@ -87,4 +79,72 @@ func init() {
 	rootCmd.AddCommand(scanCmd)
 	scanCmd.PersistentFlags().StringVar(&username, "username", "", "The user name of the provider being used.")
 	scanCmd.PersistentFlags().StringVar(&token, "token", "", "The API token of the provider being used.")
+	// rootCmd.PersistentFlags().BoolVar(&summary, "summary", false, "Displays a summary of any findings.")
+	// rootCmd.PersistentFlags().BoolVar(&detailed, "detailed", false, "Displays detailed vulnerability findings.")
+}
+
+func RenderDetails(response providers.CoordinateResponses) {
+	t := table.NewWriter()
+	t.SetOutputMirror(os.Stdout)
+	t.Style().Options.DrawBorder = false
+	t.SetColumnConfigs([]table.ColumnConfig{
+		{
+			Number:   2,
+			WidthMin: 6,
+			WidthMax: 64,
+		},
+	})
+	for _, r := range response {
+		if len(r.Vulnerabilities) > 0 {
+			t.AppendRow([]interface{}{"Package", r.Coordinates})
+			t.AppendRow([]interface{}{"Description", r.Description})
+			t.AppendRow([]interface{}{"Vulnerabilities", fmt.Sprint(len(r.Vulnerabilities))})
+			for _, v := range r.Vulnerabilities {
+				t.AppendRow([]interface{}{"CVSS Score", v.CvssScore})
+				t.AppendRow([]interface{}{"CWE", v.Cwe})
+				t.AppendRow([]interface{}{"Description", v.Description})
+			}
+			t.AppendSeparator()
+		}
+	}
+	t.Style().Options.SeparateRows = true
+	t.Render()
+}
+
+func RenderSummary(response providers.CoordinateResponses) {
+	t := table.NewWriter()
+	t.SetOutputMirror(os.Stdout)
+	t.AppendHeader(table.Row{"Type", "Name", "Version", "Severity", "Vulnerability"})
+	for _, r := range response {
+		if len(r.Vulnerabilities) > 0 {
+			purl, err := packageurl.FromString(r.Coordinates)
+			if err != nil {
+				log.Println(err)
+			}
+			for _, v := range r.Vulnerabilities {
+				t.AppendRow([]interface{}{purl.Type, purl.Name, purl.Version, v.CvssScore, v.Cwe})
+			}
+		}
+	}
+	t.SetStyle(table.StyleRounded)
+	t.SetColumnConfigs([]table.ColumnConfig{
+		{
+			Name:     "Description",
+			WidthMin: 6,
+			WidthMax: 64,
+		},
+	})
+	t.SortBy([]table.SortBy{
+		{Name: "Name", Mode: table.Dsc},
+		{Name: "Severity", Mode: table.Dsc},
+	})
+	t.SetColumnConfigs([]table.ColumnConfig{
+		{Number: 1, AutoMerge: true},
+		{Number: 2, AutoMerge: true},
+		{Number: 3, AutoMerge: true},
+	})
+	t.Style().Options.SeparateRows = true
+	t.Style().Format.Header = text.FormatDefault
+	t.Style().Color.Header = text.Colors{text.Bold}
+	t.Render()
 }
--- go.mod
+++ go.mod
@@ -5,20 +5,24 @@ go 1.19
 require (
 	github.com/CycloneDX/cyclonedx-go v0.6.0
 	github.com/devops-kung-fu/common v0.2.3
-	github.com/fatih/color v1.13.0
 	github.com/gookit/color v1.5.1
+	github.com/jedib0t/go-pretty/v6 v6.3.7
 	github.com/kirinlabs/HttpRequest v1.1.1
-	github.com/rodaine/table v1.0.1
+	github.com/package-url/packageurl-go v0.1.0
 	github.com/spf13/afero v1.9.2
 	github.com/spf13/cobra v1.5.0
+	github.com/stretchr/testify v1.8.0
 )
 
 require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/inconshreveable/mousetrap v1.0.1 // indirect
-	github.com/mattn/go-colorable v0.1.13 // indirect
-	github.com/mattn/go-isatty v0.0.16 // indirect
+	github.com/mattn/go-runewidth v0.0.13 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/rivo/uniseg v0.3.4 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/xo/terminfo v0.0.0-20210125001918-ca9a967f8778 // indirect
-	golang.org/x/sys v0.0.0-20220818161305-2296e01440c6 // indirect
+	golang.org/x/sys v0.0.0-20220823224334-20c2bfdbfe24 // indirect
 	golang.org/x/text v0.3.7 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
