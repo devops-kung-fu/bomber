fea910a48943821c89d8f6cec781d7cecb2593f9
DJ Schleen <djschleen@gmail.com>
2023-04-14 13:26:33 -0600

    Code tightening

--- providers/ossindex/OSSIndex.go
+++ providers/ossindex/OSSIndex.go
@@ -55,8 +55,7 @@ func (Provider) Scan(purls []string, credentials *models.Credentials) (packages 
 		body, _ := resp.Body()
 		if resp.StatusCode() == 200 {
 			var response []models.Package
-			err = json.Unmarshal(body, &response)
-			if err != nil {
+			if err = json.Unmarshal(body, &response); err != nil {
 				return
 			}
 			for i, pkg := range response {
@@ -81,9 +80,11 @@ func validateCredentials(credentials *models.Credentials) (err error) {
 	if credentials == nil {
 		return errors.New("credentials cannot be nil")
 	}
+
 	if credentials.Username == "" {
 		credentials.Username = os.Getenv("BOMBER_PROVIDER_USERNAME")
 	}
+
 	if credentials.Token == "" {
 		credentials.Token = os.Getenv("BOMBER_PROVIDER_TOKEN")
 	}
