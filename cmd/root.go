9b40846431aaef77cde61a6eb1916326e03cae72
DJ Schleen <djschleen@gmail.com>
2023-04-14 15:23:25 -0600

    Version bump

--- cmd/root.go
+++ cmd/root.go
@@ -16,7 +16,7 @@ import (
 )
 
 var (
-	version = "0.4.3"
+	version = "0.4.4"
 	output  string
 	//Afs stores a global OS Filesystem that is used throughout bomber
 	Afs = &afero.Afero{Fs: afero.NewOsFs()}
