acc6bd257b9850e22a604ea96815476a83e7a15a
DJ Schleen <djschleen@gmail.com>
2024-09-23 09:50:50 -0600

    Update version to 0.5.1 (#251)

--- cmd/root.go
+++ cmd/root.go
@@ -15,7 +15,7 @@ import (
 )
 
 var (
-	version = "0.5.0"
+	version = "0.5.1"
 	output  string
 	//Afs stores a global OS Filesystem that is used throughout bomber
 	Afs = &afero.Afero{Fs: afero.NewOsFs()}
