1320f51c203731f6477e0c237df4d122af882e6d
DJ Schleen <djschleen@gmail.com>
2025-01-06 21:17:25 +0000

    Adds test cases

--- providers/gad/gad_test.go
+++ providers/gad/gad_test.go
@@ -1,6 +1,7 @@
 package gad
 
 import (
+	"reflect"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -10,4 +11,22 @@ func TestInfo(t *testing.T) {
 	provider := Provider{}
 	info := provider.Info()
 	assert.Equal(t, "GitHub Advisory Database (https://github.com/advisories)", info)
+}
+
+func TestProvider_SupportedEcosystems(t *testing.T) {
+	provider := Provider{}
+	expectedEcosystems := []string{
+		"github-actions",
+		"composer",
+		"erlang",
+		"golang",
+		"maven",
+		"npm",
+		"nuget",
+		"pypi",
+		"rubygems",
+		"cargo",
+	}
+	actualEcosystems := provider.SupportedEcosystems()
+	assert.True(t, reflect.DeepEqual(expectedEcosystems, actualEcosystems), "Expected %v but got %v", expectedEcosystems, actualEcosystems)
 }
